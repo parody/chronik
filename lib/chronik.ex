05834d175759d8ce6e2c334e36397b9366894fe3
fbergeroSH <federico.bergero@surhive.com>
2017-07-05 16:34:54 -0300

    Feature: moving global type definitions to the Chronik module

--- lib/chronik.ex
+++ lib/chronik.ex
@@ -6,4 +6,13 @@ defmodule Chronik do
   @type state   :: term()
   @type event   :: term()
   @type command :: term()
+
+  @typedoc "The type of possible streams"
+  @type stream :: String.t
+
+  @typedoc "This is a boolean predicate that is used for filtering events in the bus"
+  @type predicate :: fun((term() -> boolean()))
+
+  @typedoc "The result status of all operations on the pub_sub"
+  @type result_status :: :ok | {:error, String.t}
 end
