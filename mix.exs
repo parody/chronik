60057a22fc5e1ca6c6bf1bc5b3d7ca1f6f180cbb
arpunk <arpunk@fatelectron.net>
2017-07-06 17:03:49 -0500

    Add hex metadata and misc documentation boilerplate

--- mix.exs
+++ mix.exs
@@ -1,15 +1,22 @@
 defmodule Chronik.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :chronik,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5-rc",
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test],
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+
+      # Hex
+      description: description(),
+      package: package()
     ]
   end
 
@@ -20,7 +27,28 @@ defmodule Chronik.Mixfile do
     ]
   end
 
+  defp description do
+    """
+    A lightweight event sourcing micro framework for Elixir.
+    """
+  end
+
+  defp docs do
     [
+      source_ref: "v#{@version}", main: "Chronik",
+      canonical: "http://hexdocs.pm/chronik", logo: "guides/images/chronik.png",
+      source_url: "https://github.com/surhive/chronik",
+      extras: ["guides/Getting Started.md"]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Cristian Rosa", "Federico Bergero", "Ricardo lanziano"],
+      licenses: [],
+      links: %{"GitHub" => "https://github.com/surhive/chronik"},
+      files: ~w(mix.exs README.md CHANGELOG.md lib) ++
+             ~w()
     ]
   end
 
