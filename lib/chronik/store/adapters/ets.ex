64bc58fbbf2000e1d81a11d45f08b41ba2fb2051
fbergeroSH <federico.bergero@surhive.com>
2017-07-18 11:17:16 -0300

    * The Store returns :ok on success * Also fixing an error that returned the exception

--- lib/chronik/store/adapters/ets.ex
+++ lib/chronik/store/adapters/ets.ex
@@ -24,7 +24,7 @@ defmodule Chronik.Store.Adapters.ETS do
   end
 
 
-  defp do_append(stream, events, opts  \\ [version: :any]) do
+  defp do_append(stream, events, record_version, opts  \\ [version: :any]) do
     {current_records, current_offset} =
       case get_stream(stream) do
         :not_found ->
@@ -33,7 +33,7 @@ defmodule Chronik.Store.Adapters.ETS do
           # FIXME: Use another data type for storing events instead of List
           {records, records |> List.last |> Map.get(:offset)}
       end
-    new_records = from_enum(stream, current_offset + 1, events)
+    new_records = from_enum(stream, current_offset + 1, events, record_version)
     Logger.debug ["[#{inspect __MODULE__}<#{inspect stream}>] ",
                   "appending events: #{inspect events}."]
     case Keyword.get(opts, :version) do
@@ -56,7 +56,9 @@ defmodule Chronik.Store.Adapters.ETS do
     GenServer.call(@name, {:fetch, stream, offset})
   end
 
-  def handle_call({:fetch, stream, offset}, _from, public_topics) do
+  def handle_call({:fetch, stream, offset}, _from, state) do
+    public_topics = Keyword.fetch!(state, :public_topics)
+    record_version = Keyword.fetch!(state, :record_version)
     result =
       case get_stream(stream) do
         :not_found ->
@@ -70,10 +72,12 @@ defmodule Chronik.Store.Adapters.ETS do
             |> Enum.to_list
           {:ok, offset + length(new_records), new_records}
       end
-    {:reply, result, public_topics}
+    {:reply, result, state}
   end
 
-  def handle_call({:append, stream, events, opts}, _from, public_topics) do
+  def handle_call({:append, stream, events, opts}, _from, state) do
+    public_topics = Keyword.fetch!(state, :public_topics)
+    record_version = Keyword.fetch!(state, :record_version)
     # First write the public events
     public_records =
       if map_size(public_topics) > 0 do
@@ -85,7 +89,7 @@ defmodule Chronik.Store.Adapters.ETS do
             event_type = event.__struct__
             case Map.fetch(public_topics, event_type) do
               {:ok, topic} ->
-                {:ok, _, new_records} = do_append(topic, [event])
+                {:ok, _, new_records} = do_append(topic, [event], record_version)
                 new_records ++ records
               _ -> records
             end
@@ -96,13 +100,13 @@ defmodule Chronik.Store.Adapters.ETS do
       end
     # Then write the events on the aggregate stream
     reply =
-      case do_append(stream, events, opts) do
+      case do_append(stream, events, record_version, opts) do
         {:ok, offset, aggregate_records} ->
           {:ok, offset, public_records ++ aggregate_records}
 
         error -> error
       end
-    {:reply, reply, public_topics}
+    {:reply, reply, state}
   end
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: @name)
@@ -113,7 +117,9 @@ defmodule Chronik.Store.Adapters.ETS do
   def init([_store, opts]) do
     try do
       :ets.new(@table, [:set, :named_table, :public])
-      {:ok, Keyword.get(opts, :public_topics, %{})}
+      public_topics = Keyword.get(opts, :public_topics, %{})
+      record_version = Keyword.get(opts, :record_version, "1")
+      {:ok, [public_topics: public_topics, record_version: record_version]}
     rescue
       _ -> {:stop, {:error, "event store already started"}}
     end
@@ -134,16 +140,17 @@ defmodule Chronik.Store.Adapters.ETS do
     {:ok, last.offset, records}
   end
 
-  defp append_records(stream, current_records, new_records) do
-    true = :ets.insert(@table, {stream, current_records ++ new_records})
+  defp append_records(stream, current_records, records) do
+    new_records = current_records ++ records
+    true = :ets.insert(@table, {stream, new_records})
     last = List.last(new_records)
-    {:ok, last.offset, new_records}
+    {:ok, last.offset, records}
   end
 
-  defp from_enum(stream, offset, events) do
+  defp from_enum(stream, offset, events, record_version) do
     {records, _} = Enum.reduce(events, {[], offset},
       fn (elem, {records, next_offset}) ->
-        {[EventRecord.create(stream, next_offset, elem) | records], next_offset + 1}
+        {[EventRecord.create(stream, next_offset, elem, record_version) | records], next_offset + 1}
       end)
     Enum.reverse(records)
   end
